b4db052a08bb0be9b68c3faf9ccb45b4e281665d
Joao Duarte <joao.duarte@fulltime.com.br>
2018-10-17 11:43:28 -0300

    Commit inicial

---
+++ src/ios/HKVideoPlayer.swift
@@ -0,0 +1,104 @@
+//
+//  VideoViewController.swift
+//  Doctodoor
+//
+//  Created by Hardik on 22/08/15.
+//  Copyright (c) 2015 PRAXINFO. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+import AVKit
+import AVFoundation
+//import Cordova
+
+@objc(HKVideoPlayer) class HKVideoPlayer: CDVPlugin {
+    
+    var playerViewController:AVPlayerViewController!
+    
+    override func pluginInitialize() {
+        playerViewController = AVPlayerViewController()
+    }
+    
+    @objc(play:)
+    func play(command:CDVInvokedUrlCommand){
+        guard let url = command.argument(at: 0) as? String else {
+            return
+        }
+
+        let videoURL = URL(string: url)
+        let player = AVPlayer(url: videoURL!)
+        self.present(player: player)
+    }
+    
+    @objc(playLocal:)
+    func playLocal(command:CDVInvokedUrlCommand){
+        guard let name = command.argument(at: 0) as? String else {
+            return
+        }
+        let ext = name.fileExtension()
+        let file = name.fileName()
+        let path = Bundle.main.path(forResource: file, ofType: ext, inDirectory: "www/assets/video")
+        let videoURL = URL(fileURLWithPath: path!)
+        let player = AVPlayer(url: videoURL)
+        self.present(player: player)
+    }
+    
+    @objc(playPlaylist:)
+    func playPlaylist(command:CDVInvokedUrlCommand){
+        guard let playlist = command.argument(at: 0) as? Array<Any> else {
+            return
+        }
+        
+        var playlistItem: [AVPlayerItem] = []
+        for item in playlist {
+            playlistItem.append(AVPlayerItem(url: URL(string: item as! String)!))
+        }
+        presentPlayList(queue: playlistItem)
+    }
+
+    private func present(player:AVPlayer){
+        
+        playerViewController.player = player
+        DispatchQueue.main.async {
+            self.viewController.present(self.playerViewController, animated: true) {
+                self.playerViewController.player!.play()
+            }
+        }
+    }
+    
+    private func presentPlayList(queue: [AVPlayerItem]) {
+        let playlistItem = AVQueuePlayer(items: queue)
+        self.playerViewController.player = playlistItem
+
+        DispatchQueue.main.async {
+            self.viewController.present(self.playerViewController, animated: true) {
+                self.playerViewController.player?.play()
+            }
+        }
+    }
+    
+    deinit {
+        self.playerViewController = nil
+    }
+}
+extension String {
+    
+    func fileName() -> String {
+        
+        if let fileNameWithoutExtension = NSURL(fileURLWithPath: self).deletingPathExtension?.lastPathComponent {
+            return fileNameWithoutExtension
+        } else {
+            return ""
+        }
+    }
+    
+    func fileExtension() -> String {
+        
+        if let fileExtension = NSURL(fileURLWithPath: self).pathExtension {
+            return fileExtension
+        } else {
+            return ""
+        }
+    }
+}
